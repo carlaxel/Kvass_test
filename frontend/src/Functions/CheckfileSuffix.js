4ce1186f27c243b396b1b8c46342afe22669b3db
Carl-Axel Hallgren <carl-axel.hallgren@enfogroup.com>
2020-01-19 20:55:52 +0100

    fixed edgecase for checksuffix

--- frontend/src/Functions/CheckfileSuffix.js
+++ frontend/src/Functions/CheckfileSuffix.js
@@ -1,5 +1,6 @@
 export default function checkSuffix(filename) {
-  let [_file, suffix] = filename.split(".");
+  let fileArray = filename.split(".");
+  let suffix = fileArray[fileArray.length - 1];
   suffix = suffix.toLowerCase();
   console.log(suffix);
   return suffix === "jpg" || suffix === "jpeg" || suffix === "png";
